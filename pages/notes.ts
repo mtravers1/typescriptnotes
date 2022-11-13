07f900c745bc5141edc8dd8d46c071fa31d04224
Michael Travers <michael.travers@udc.edu>
2022-11-13 12:50:43 -0500

    notes on functions and interfactes

---
+++ pages/notes.ts
@@ -0,0 +1,11 @@
+const button = doucment.querySelector('button')
+const input1= document.getElementById('num1')
+const input2= document.getElementById('num2')
+
+function add(num1, num2){
+    return num1+num2
+}
+
+button.addEventlistener('click', function(){
+    console.log(add(input1.value,input2.value))
+})
